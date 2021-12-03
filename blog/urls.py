0c1d5efd0cf9204d7f48f74a58dd8578db420191
yash dhawan <yashdhawan@yashs-MacBook-Pro.local>
2021-12-04 04:23:57 +0530

    Added Static Files

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path 
-
+from . import views
 urlpatterns = [
-    path(""),
-    path("posts"),
-    path("posts/<slug>") #/posts/my-first-post
+    path("",views.starting_page, name="starting-page"),
+    path("posts",views.posts,name="posts-page"),
+    path("posts/<slug>",views.post_details,name="post-detail-page") #/posts/my-first-post
 ]
